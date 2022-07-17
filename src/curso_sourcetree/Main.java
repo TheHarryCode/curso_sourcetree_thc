d06ab9ef9a69f65bc8f110b64eac8aa0c4f951ec
Harry Mesa <theharrycode@gmail.com>
2022-07-16 19:37:41 -0500

    First Commit

---
+++ src/curso_sourcetree/Main.java
@@ -0,0 +1,15 @@
+package curso_sourcetree;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Main Class");
+		System.out.println("Curso de Sourcetree");
+		
+		Persona per = new Persona();
+		per.setApellido("Mesa");
+		
+	}
+
+}
